045afaaff6cec48d7eeb8ffbd4c181827f838800
jwnas <jns.lima@outlook.com>
2025-01-03 21:41:56 -0300

    objetos

---
+++ Trilha Backend/aula - 06/objetos.js
@@ -0,0 +1,12 @@
+const animal = {
+    especie: "Lobo",
+    habitate: "Floresta",
+    expectativaDeVida: "20 anos",
+    terrestre: true
+};
+
+console.log(animal);
+console.log(animal.especie);
+
+animal.populacao = 600_000_000; // ou animal.populacao = 6 * 10 ** 9
+console.log(animal.populacao);
